e2e5d6e37b80cfd2d9f0d9aa14bcf0351e0fb5fa
barathan ganesh <demongod250299@gmail.com>
2020-06-26 16:07:30 +0530

    added customer form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Button } from 'antd';
 import { HomeFilled } from '@ant-design/icons';
 import './App.css';
+import CustomerForm from './components/customerForm';
 
 function App() {
   return (
+    <React.Fragment>
     <Button type='primary' icon={<HomeFilled /> } > Home </Button>
+    <CustomerForm /></React.Fragment>
   );
 }
 
---
+++ src/components/customerForm.js
@@ -0,0 +1,116 @@
+import { Form, Input, Button, Checkbox,Select } from 'antd';
+import React, { useState } from "react";
+import  AvatarLoader  from "./avatarLoader";
+
+const layout = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 8 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 4, span: 8 },
+};
+
+const { Option } = Select;
+
+const CustomerForm = () => {
+  const [values,setValues] = useState({});
+  const onFinish = values => {
+    console.log('Success:', values);
+    setValues(values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const initialValues = {
+      fname:"",
+      lname:"",
+      address:"",
+      phno:null,
+      avatar:"",
+      customizeGender:""
+  }
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={initialValues}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="First Name"
+        name="fname"
+        rules={[{ required: true, message: 'Please enter First name' }]}
+      >
+        <Input.name />
+      </Form.Item>
+      
+      <Form.Item
+        label="Last Name"
+        name="lname"
+        rules={[{ required: true, message: 'Please enter Last name' }]}
+      >
+        <Input.name />
+      </Form.Item>
+      
+      <Form.Item
+        label="Address"
+        name="address"
+        rules={[{ required: true, message: 'Please enter your address' }]}
+      >
+        <Input />
+      </Form.Item>
+      
+      <Form.Item
+        label="Contact"
+        name="phno"
+        rules={[{ required: true, message: 'Please enter your contact no.' }]}
+      >
+        <Input type='tel' />
+      </Form.Item>
+
+      <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+        <Select
+          placeholder="Select a option and change input text above"
+          allowClear
+        >
+          <Option value="male">male</Option>
+          <Option value="female">female</Option>
+          <Option value="other">other</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item
+        noStyle
+        shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+      >
+        {({ getFieldValue }) => {
+          return getFieldValue('gender') === 'other' ? (
+            <Form.Item name="customizeGender" label="Custom Gender" rules={[{ required: true }]}>
+              <Input />
+            </Form.Item>
+          ) : null;
+        }}
+      </Form.Item>
+
+      <Form.Item
+        label="Avatar"
+        name="avatar"
+      >
+        <AvatarLoader />
+        <h4>if no image is uploaded , default is set as avatar</h4>
+      </Form.Item>
+
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default CustomerForm;
